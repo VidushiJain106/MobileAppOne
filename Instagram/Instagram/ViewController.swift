05fd17d4feaf2dee9a9e4927528d24731767b1d8
Vidushi Jain <vidushijain@Vidushis-MacBook-Pro.local>
2021-09-04 19:16:17 -0400

    Add few UI elements to pick a photo. Add code to select and display a photo

--- Instagram/Instagram/ViewController.swift
+++ Instagram/Instagram/ViewController.swift
@@ -8,27 +8,36 @@
 import UIKit
 import Parse
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = image
+            
+        } else {
+            print("There was a problem getting the image")
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    @IBAction func chooseImage(_ sender: Any) {
+        let imagePickercontroller = UIImagePickerController()
+        imagePickercontroller.delegate = self
+        imagePickercontroller.sourceType = UIImagePickerController.SourceType.photoLibrary;
+        imagePickercontroller.allowsEditing = false
+        self.present(imagePickercontroller, animated:true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        let gameScore = PFObject(className:"GameScore")
-        gameScore["score"] = 1337
-        gameScore["playerName"] = "Sean Plott"
-        gameScore["cheatMode"] = false
-        gameScore.saveInBackground { (succeeded, error)  in
-            if (succeeded) {
-                print("Success!")
-            } else {
-                print("Failed!")
-            }
-        }
-
+        
         
     }
-
 
 }
 
