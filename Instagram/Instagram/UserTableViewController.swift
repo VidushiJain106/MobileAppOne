3cf0c41550530f7a6452910b0077b4b3f88f3b86
Vidushi Jain <vidushijain@Vidushis-MacBook-Pro.local>
2021-09-05 13:23:40 -0400

    Create a UserTable as well as the logout flow

---
+++ Instagram/Instagram/UserTableViewController.swift
@@ -0,0 +1,122 @@
+//
+//  UserTableViewController.swift
+//  Instagram
+//
+//  Created by Vidushi Jain on 9/5/21.
+//
+
+import UIKit
+import Parse
+
+class UserTableViewController: UITableViewController {
+    
+    var usernames = [""]
+    
+    @IBAction func logoutUser(_ sender: Any) {
+        print("logout function!")
+        PFUser.logOut()
+        performSegue(withIdentifier: "logoutSegue", sender: self)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let query = PFUser.query()
+        
+        query?.whereKey("username", notEqualTo: PFUser.current()?.username)
+        
+        query?.findObjectsInBackground(block: { (users, error) in
+            if error != nil {
+                print(error)
+            } else if let users = users {
+                
+                self.usernames.removeAll()
+                
+                for object in users {
+                    if let user = object as? PFUser {
+                        let usernameArray = user.username!.components(separatedBy: "@")
+                        self.usernames.append(usernameArray[0])
+                    }
+                }
+            }
+            
+            self.tableView.reloadData()
+        })
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return usernames.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        
+        cell.textLabel?.text = usernames[indexPath.row]
+        
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
