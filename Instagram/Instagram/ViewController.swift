76fc8b9649c2fc3b46bc7503d96ebcdd523414f6
Vidushi Jain <vidushijain@Vidushis-MacBook-Pro.local>
2021-09-05 00:03:44 -0400

    Create a fully functioning login page. Also connect it to ParseServer to save and read users

--- Instagram/Instagram/ViewController.swift
+++ Instagram/Instagram/ViewController.swift
@@ -8,36 +8,93 @@
 import UIKit
 import Parse
 
-class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+class ViewController: UIViewController{
 
-    @IBOutlet weak var imageView: UIImageView!
+    var signupModeActive = true
     
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        
-        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-            imageView.image = image
-            
-        } else {
-            print("There was a problem getting the image")
-        }
-        
-        self.dismiss(animated: true, completion: nil)
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    func displayAlert(title:String, message:String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: { (action) in
+                self.dismiss(animated:true, completion: nil)
+            }))
+        self.present(alert, animated: true)
     }
-    @IBAction func chooseImage(_ sender: Any) {
-        let imagePickercontroller = UIImagePickerController()
-        imagePickercontroller.delegate = self
-        imagePickercontroller.sourceType = UIImagePickerController.SourceType.photoLibrary;
-        imagePickercontroller.allowsEditing = false
-        self.present(imagePickercontroller, animated:true, completion: nil)
+    
+    @IBOutlet weak var switchLoginModeLabel: UILabel!
+    
+    @IBOutlet weak var singUpOrLoginButton: UIButton!
+    @IBAction func signUpOrLogin(_ sender: Any) {
+        if email.text == "" || password.text == "" {
+            displayAlert(title:"Error in form", message:"Please enter a valid email and password")
+        } else {
+            
+            let activityIndicator = UIActivityIndicatorView(frame: CGRect(x:0, y:0, width:50, height:50))
+            activityIndicator.center = self.view.center
+            activityIndicator.hidesWhenStopped = true
+            activityIndicator.style = UIActivityIndicatorView.Style.large
+            view.addSubview(activityIndicator)
+            activityIndicator.startAnimating()
+            self.view.isUserInteractionEnabled = false
+            
+            if (signupModeActive) {
+                let user = PFUser()
+                user.username = email.text
+                user.password = password.text
+                user.email = email.text
+
+                user.signUpInBackground {(succeeded: Bool, error: Error?) -> Void in
+                    activityIndicator.stopAnimating()
+                    self.view.isUserInteractionEnabled = true
+                    
+                    if let error = error {
+                        let errorString = error.localizedDescription
+                        self.displayAlert(title: "Error signing up", message: errorString)
+                        // Show the errorString somewhere and let the user try again.
+                    } else {
+                        print("Signed Up")
+                        // Hooray! Let them use the app now.
+                    }
+                }
+            } else {
+                PFUser.logInWithUsername(inBackground:email.text!, password:password.text!) {
+                  (user: PFUser?, error: Error?) -> Void in
+                  activityIndicator.stopAnimating()
+                  self.view.isUserInteractionEnabled = true
+                  if user != nil {
+                    print("Logged in")
+                  } else {
+                    if error != nil {
+                        let errorString = error!.localizedDescription
+                        self.displayAlert(title: "Error signing up", message: errorString)
+                    } else {
+                        self.displayAlert(title: "Error signing up", message: "Unknown Error")
+                    }
+                  }
+                }
+            }
+        }
+    }
+    @IBOutlet weak var switchLoginModeButton: UIButton!
+    @IBAction func switchLoginMode(_ sender: Any) {
+        if (signupModeActive) {
+            signupModeActive = false
+            singUpOrLoginButton.setTitle("Login", for: [])
+            switchLoginModeButton.setTitle("Sign Up", for: [])
+            switchLoginModeLabel.text = "Don't have an account?"
+        } else {
+            signupModeActive = true
+            singUpOrLoginButton.setTitle("Sign Up", for: [])
+            switchLoginModeButton.setTitle("Login", for: [])
+            switchLoginModeLabel.text = "Already have an account?"
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        
-        
     }
 
 }
-
